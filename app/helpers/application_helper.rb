d976ae56f8649f407ebf871a74f12f78ed4b3864
Konstantin Kosmatov <key@kosmatov.su>
2012-12-04 17:28:38 +0400

    move pages scope from view to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
     content_tag(:span, user.human_state_name, { class: cls })
   end
 
+  def system_pages
+    Page.where(slug: configus.page_slugs).send("asc_by_name")
+  end
 end
