855343b9f1ede6543df85f33d22dce478532d921
Konstantin Kosmatov <key@kosmatov.su>
2012-12-10 19:42:56 +0400

    simple form base initializer

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -17,6 +17,7 @@ SimpleForm.setup do |config|
     b.use :human_state_name
     b.use :label
     b.wrapper :tag => 'div', :class => 'controls' do |ba|
+      ba.use :input
       ba.use :error, wrap_with: { :tag => 'span', :class => 'help-inline' }
       ba.use :hint,  wrap_with: { :tag => 'p', :class => 'help-block' }
     end
