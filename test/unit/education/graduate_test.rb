3d2e45ada6f8c4ea8a3e777c517cc9549e7197b5
Konstantin Kosmatov <key@kosmatov.su>
2012-12-17 13:30:08 +0400

    fix tests

--- test/unit/education/graduate_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Education::GraduateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/web/admin/education_graduates_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Web::Admin::EducationGraduatesHelperTest < ActionView::TestCase
-end
