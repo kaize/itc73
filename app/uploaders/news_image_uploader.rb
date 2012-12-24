dc14c3c7c819f817241628b6bd3dbbcc3c42161b
PanfilovDenis <panf.denis@gmail.com>
2012-12-24 12:35:28 +0000

    change upload path

--- app/uploaders/news_image_uploader.rb
+++ app/uploaders/news_image_uploader.rb
@@ -6,7 +6,7 @@ class NewsImageUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process resize_to_limit: [300, 400]
