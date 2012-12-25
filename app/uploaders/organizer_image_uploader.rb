d19b4a54093dc81436129470a516761670389dfd
PanfilovDenis <panf.denis@gmail.com>
2012-12-25 11:14:15 +0000

    delete resize organization picture

--- app/uploaders/organizer_image_uploader.rb
+++ app/uploaders/organizer_image_uploader.rb
@@ -8,8 +8,6 @@ class OrganizerImageUploader < CarrierWave::Uploader::Base
     "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [120, 80]
-
   def extension_white_list
    %w(jpg jpeg gif png)
   end
