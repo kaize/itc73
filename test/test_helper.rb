912e2371b87c7416fe2f50d0ecbfed1bffbc6926
PanfilovDenis <panf.denis@gmail.com>
2012-12-26 09:21:34 +0000

    fix support coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,14 @@
 require 'simplecov'
 SimpleCov.start('rails') if ENV["COVERAGE"]
+
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV["RAILS_ENV"] = "test"
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-require 'coveralls'
-Coveralls.wear!
 
 class ActiveSupport::TestCase
   include AuthHelper
