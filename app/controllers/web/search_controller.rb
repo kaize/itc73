595c34d548b5b50f62b4c4f52bc688667611e643
PanfilovDenis <panf.denis@gmail.com>
2013-01-14 14:36:13 +0000

    refactoring

--- app/controllers/web/search_controller.rb
+++ app/controllers/web/search_controller.rb
@@ -1,9 +1,7 @@
 class Web::SearchController < Web::ApplicationController
   def index
-  	@query = params[:term].present? ? params[:term] : nil 
+  	@query = params[:term] 
   	@results = []
-  	if @query
-      @results = Course.search @query
-    end
+    @results = Course.search @query
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -36,4 +36,8 @@ class Course < ActiveRecord::Base
   def can_destroy?
     persisted?
   end
+
+  def to_s
+    name
+  end
 end
