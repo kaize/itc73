86a1637f75a03e22568990d3604ae739c63dbf2f
PanfilovDenis <panf.denis@gmail.com>
2013-01-14 13:46:33 +0000

    fix capistrano deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,8 +32,8 @@ namespace :sphinx do
   end
 end
 
-before 'deploy:update_code', 'thinking_sphinx:stop'
 before 'deploy:finalize_update', 'deploy:symlink_db'
+before 'deploy:update_code', 'thinking_sphinx:stop'
 after 'deploy:finalize_update', 'sphinx:symlink_indexes'
 after 'deploy:update_code', 'thinking_sphinx:start'
 after 'deploy:restart', 'unicorn:stop'
