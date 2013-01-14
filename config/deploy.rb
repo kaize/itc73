7bac50f1892c31c77ea9ec860d31b90d5cf77f8f
PanfilovDenis <panf.denis@gmail.com>
2013-01-14 10:00:50 +0000

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :rvm_type, :user
 set :stages, %w(staging)
 set :default_stage, "staging"
 set :rvm_ruby_string, '1.9.3'
-#set :whenever_command, "bundle exec whenever" # update crontab
+set :whenever_command, "bundle exec whenever" # update crontab
 
 set :use_sudo, false
 
