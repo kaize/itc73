fcf910ce9e2369264b2066f91acf9f9ef6051d2a
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-01-23 18:38:50 +0400

    Uri valid message

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -4,10 +4,11 @@ class Organizer < ActiveRecord::Base
   attr_accessible :image, :name, :order_at, :url
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :url, :url => true
+  validates :url, :url => {:allow_blank => true, :message => "Uri is not valid"}, :length => { :maximum =>255 }
   mount_uploader :image, OrganizerImageUploader
 
   def can_destroy?
     persisted?
   end
 end
+
