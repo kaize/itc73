02935b244af6d56084d5b77be150fccc6e2d528b
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-01-24 11:57:09 +0400

    Adding regular expression, no other ideas

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -3,8 +3,8 @@ class Organizer < ActiveRecord::Base
 
   attr_accessible :image, :name, :order_at, :url
 
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :url, :url => {:allow_blank => true, :message => "Uri is not valid"}, :length => { :maximum =>255 }
+  validates :name, :presence=> true, :length => { :maximum => 255 }
+  validates :url, :url => true, :format => {:with => /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/}
   mount_uploader :image, OrganizerImageUploader
 
   def can_destroy?
