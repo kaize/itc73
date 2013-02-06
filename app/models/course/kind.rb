df42da092a2b57211d3a938672e605e1f8c1f982
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-02-06 16:24:13 +0400

    Adding restriction for levels and kinds of courses

--- app/models/course/kind.rb
+++ app/models/course/kind.rb
@@ -1,8 +1,7 @@
 class Course::Kind < ActiveRecord::Base
   include Course::KindRepository
 
-  has_many :courses
-
+  has_many :courses, :dependent => :restrict
   attr_accessible :name, :order_at
 
   validates :name, presence: true, length: { maximum: 255 }
