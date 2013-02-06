3ba01b8d9cab13ea0fe65f2d73b8836f602b91a1
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-02-06 23:42:19 +0400

    Rescueing in right way

--- app/controllers/web/admin/course_kinds_controller.rb
+++ app/controllers/web/admin/course_kinds_controller.rb
@@ -47,11 +47,10 @@ class Web::Admin::CourseKindsController < Web::Admin::ApplicationController
     begin
       kind.destroy
       flash_success
-    rescue ActiveRecord::DeleteRestrictionError => e
-      kind.errors.add(:base, e)
-      flash[:error] = "#{t('flash.delete_restriction_error')}"
+    rescue ActiveRecord::DeleteRestrictionError
+      flash_error now: false
     ensure
-      redirect_to action: :index
+      redirect_to :back
     end
   end
 end
