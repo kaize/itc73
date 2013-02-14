eda13805661980db944bf679947e81eed129f1f0
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-02-14 18:23:48 +0400

    Adding default_image

--- app/uploaders/news_image_uploader.rb
+++ app/uploaders/news_image_uploader.rb
@@ -4,7 +4,7 @@ class NewsImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   def default_url
-    "/images/fallback" + [version_name, "default.png"].compact.join('_')
+    "fallback/" + [version_name, "default.png"].compact.join('_')
   end
   storage :file
 
