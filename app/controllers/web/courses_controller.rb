20001c4953799968ad1b792d65539e8c1c99f2a4
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-02-22 17:30:34 +0400

    Adding titles

--- app/controllers/web/courses_controller.rb
+++ app/controllers/web/courses_controller.rb
@@ -3,10 +3,12 @@ class Web::CoursesController < Web::ApplicationController
 
   def index
     @courses = Course.page(params[:page])
+    title t('.pages_in_titles.courses') 
   end 
 
   def show
     @course = Course.find params[:id]
     add_breadcrumb @course.name, course_path(@course)
+    title @course.name
   end
 end
