179aa831d3a6cb8c6c54d1dcc002081c3088e75a
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-02-22 17:30:34 +0400

    Adding titles

--- app/controllers/web/news_controller.rb
+++ app/controllers/web/news_controller.rb
@@ -3,11 +3,13 @@ class Web::NewsController < Web::ApplicationController
 
   def index
     @news = News.published.by_published_at.page(params[:page]).decorate
+    title t('.pages_in_titles.news') 
   end
 
   def show
     news = News.find params[:id]
     @news = news.decorate
     add_breadcrumb @news.name, news_path(@news)
+    title @news.name
   end
 end
