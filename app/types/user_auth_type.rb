a696ba69ea610f761717028d736002bcd5723f7c
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-03-13 12:29:26 +0400

    Vkontakte bugs fix

--- app/types/user_auth_type.rb
+++ app/types/user_auth_type.rb
@@ -3,7 +3,6 @@ class UserAuthType < User
 
   attr_accessible :personal_data_processing
 
-  validates :password, presence: true
   validates :birthday, presence: true
   validates :university, presence: true, length: { maximum: 255 }
   validates :edu_year_end, presence: true
