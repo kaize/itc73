2aa0785fbc86379c4b2bab993a5b4bb0a9427cf0
Alexey Frank <alexeyfrank@gmail.com>
2013-05-15 16:13:39 +0400

    fix production environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :rvm_type, :user
-set :stages, %w(staging)
+set :stages, %w(production staging)
 set :default_stage, "staging"
 set :rvm_ruby_string, '1.9.3'
 set :whenever_command, "bundle exec whenever" # update crontab
---
+++ config/deploy/production.rb
@@ -0,0 +1,16 @@
+set :rails_env, "production"
+set :unicorn_env, "production"
+set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
+set :branch do
+  raise "Use tags: TAG=v2" unless ENV['TAG']
+  ENV['TAG']
+end
+set :user, 'itc73_production'
+set :keep_releases, 5
+
+role :web, '62.76.191.134'
+role :app, '62.76.191.134'
+role :db,  '62.76.191.134', :primary => true
+
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
