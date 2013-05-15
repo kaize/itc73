533eff7c22e7c345ee47446b7e4f2c449d51ee44
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-05-15 15:27:18 +0400

    Adding basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :title
   
+  before_filter :basic_auth if Rails.env.staging?
   before_filter :sign_in_by_token
 
   private
